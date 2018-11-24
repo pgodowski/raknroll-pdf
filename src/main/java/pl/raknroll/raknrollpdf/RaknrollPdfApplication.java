582af2c4cc550544a8a89fd727273dfc87ba7f7f
Piotr Korlaga <piotrkorlaga@gmail.com>
2018-11-24 18:06:12 +0100

    init

---
+++ src/main/java/pl/raknroll/raknrollpdf/RaknrollPdfApplication.java
@@ -0,0 +1,11 @@
+package pl.raknroll.raknrollpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaknrollPdfApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RaknrollPdfApplication.class, args);
+	}
+}
